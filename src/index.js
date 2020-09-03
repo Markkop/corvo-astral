56a061aa7cfe7046ddef47d5b8870afbb14f2d7b
Mark Kop <mk.marcelo.mk@gmail.com>
2020-09-02 21:32:10 -0300

    fix: calculateAttackDamage import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
-const { getAlmanaxBonus } = require('./helpers')
+const { getAlmanaxBonus, calculateAttackDamage } = require('./helpers')
 require('dotenv').config()
 
 const client = new Discord.Client()
