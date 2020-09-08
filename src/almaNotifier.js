60c494b44f7b79b9f0345916d35eac505c8563c0
Mark Kop <mk.marcelo.mk@gmail.com>
2020-09-08 10:46:10 -0300

    fix: notify .alma on all channels named almanax

--- src/almaNotifier.js
+++ src/almaNotifier.js
@@ -2,16 +2,22 @@ import Discord from 'discord.js'
 import { getAlmanaxBonus } from './commands/alma'
 
 /**
- * Send a message with today's almanax bonus to a channel.
+ * Send a message with today's almanax bonus to all channels named "almanax".
  */
 function notifyAlmanaxBonus () {
   const client = new Discord.Client()
   client.login(process.env.DISCORD_BOT_TOKEN)
   client.on('ready', async () => {
-    const channel = client.channels.cache.find(channel => channel.name.includes('almanax'))
-    if (channel) {
-      await getAlmanaxBonus({ channel })
+    const channels = client.channels.cache.map(channel => channel.id)
+
+    for (let index = 0; index < channels.length; index++) {
+      const channel = client.channels.cache.get(channels[index])
+
+      if (channel.name.includes('almanax')) {
+        await getAlmanaxBonus({ channel })
+      }
     }
+
     client.destroy()
   })
 }
