0f38c6f5971301e2d081e61498f5ac00df848f3e
Mark Kop <mk.marcelo.mk@gmail.com>
2020-10-05 22:24:43 -0300

    fix: try catch alma errors

--- src/commands/alma.js
+++ src/commands/alma.js
@@ -1,6 +1,7 @@
 import events from '../../data/almanaxBonuses'
 import { getArgumentsAndOptions } from '../utils/message'
 import { setLanguage } from '../utils/language'
+import { handleMessageError } from '../utils/handleError'
 import str from '../stringsLang'
 
 /**
@@ -10,24 +11,28 @@ import str from '../stringsLang'
  * @returns {Promise<object>}
  */
 export function getAlmanaxBonus (message) {
-  const { options } = getArgumentsAndOptions(message, '=')
-  const lang = setLanguage(options, message.guild.id)
-  const today = new Date(Date.now())
-  const todayEvent = events.find(event => {
-    const eventFirstDate = new Date(event.firstDate)
-    const diffTime = Math.abs(today - eventFirstDate)
-    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24))
-    return diffDays % 5 === 0
-  })
+  try {
+    const { options } = getArgumentsAndOptions(message, '=')
+    const lang = setLanguage(options, message.guild.id)
+    const today = new Date(Date.now())
+    const todayEvent = events.find(event => {
+      const eventFirstDate = new Date(event.firstDate)
+      const diffTime = Math.abs(today - eventFirstDate)
+      const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24))
+      return diffDays % 5 === 0
+    })
 
-  const number = Math.floor(Math.random() * 3)
-  const image = todayEvent.images[number]
-  const embed = {
-    color: '#40b2b5',
-    title: todayEvent.name[lang],
-    description: `${str.capitalize(str.todaysAlma[lang])}: ${todayEvent.text[lang]}`,
-    image: { url: image },
-    timestamp: new Date()
+    const number = Math.floor(Math.random() * 3)
+    const image = todayEvent.images[number]
+    const embed = {
+      color: '#40b2b5',
+      title: todayEvent.name[lang],
+      description: `${str.capitalize(str.todaysAlma[lang])}: ${todayEvent.text[lang]}`,
+      image: { url: image },
+      timestamp: new Date()
+    }
+    return message.channel.send({ embed })
+  } catch (error) {
+    handleMessageError(error, message)
   }
-  return message.channel.send({ embed })
 }
