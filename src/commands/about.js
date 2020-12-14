9004ce863237a812f3d6518d97d027eff4fac52e
Marcelo Kopmann <16388408+Markkop@users.noreply.github.com>
2020-12-14 15:22:16 -0300

    echo: update about (#33)
    * fix: default config on tests
    
    * echo: update .about with discord link
    
    * fix: readme typos

--- src/commands/about.js
+++ src/commands/about.js
@@ -1,11 +1,15 @@
-export const aboutText = `**Corvo Astral** is a Discord Bot thar serves as a helper for **Wakfu**, a Ankama's MMORPG.
+export const aboutText = `**Corvo Astral** is a Discord Bot that serves as a helper for **Wakfu**, an Ankama's MMORPG.
 
 Made with Javascript and NodeJS and hosted on Heroku, this bot uses data gathered from Wakfu's CDN and Website as well Method's public API.
 
-Created by [Mark Kop](https://github.com/Markkop) (Markx)
+Created by [Mark Kop](https://github.com/Markkop) (Markx - Rubilax)
 
 Want to contribute? This project is open source and it's available on Github
-Check it out: [https://github.com/Markkop/corvo-astral](https://github.com/Markkop/corvo-astral)`
+Check it out: [https://github.com/Markkop/corvo-astral](https://github.com/Markkop/corvo-astral)
+
+Join the bot's discord server to get more help, report bugs or discover new features:
+https://discord.gg/aX6j3gM8HC    
+`
 
 /**
  * Send a message with information about this bot.
