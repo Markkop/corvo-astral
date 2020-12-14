4b8c2f5a6e60658e22bac77f3547ae9dfe6f880a
Marcelo Kopmann <mk.marcelo.mk@gmail.com>
2020-12-14 15:03:53 -0300

    fix: default config on tests

--- tests/config.test.js
+++ tests/config.test.js
@@ -30,7 +30,7 @@ describe('configGuild', () => {
   "prefix": ".",
   "almanaxChannel": "almanax",
   "partyChannel": "listagem-de-grupos",
-  "buildPreview": "disabled"
+  "buildPreview": "enabled"
 }
 \`\`\``,
       title: 'No custom config found. Using default'
@@ -48,7 +48,7 @@ describe('configGuild', () => {
   "prefix": ".",
   "almanaxChannel": "almanax",
   "partyChannel": "listagem-de-grupos",
-  "buildPreview": "disabled"
+  "buildPreview": "enabled"
 }
 \`\`\``,
       title: 'Config for "GuildName"'
