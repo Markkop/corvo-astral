30a197f6b6b07f961f11faf5ee17eb2c4f50e014
Marcelo Kopmann <marcelokopmann@gmail.com>
2021-09-16 17:19:02 -0300

    feat: server to host github badge

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import path from 'path'
+
+const PORT = process.env.PORT || 8080
+
+export default function initiateServer() {
+  try {
+    const app = express()
+    app.use(express.json())
+  
+    app.get('/badge', (request, response) => {
+      response.sendFile(path.resolve(__dirname + '/../data/generated/serversBadge.json'))
+    })
+    
+    app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) })
+  } catch (error) {
+    console.error(error)
+  }
+}
