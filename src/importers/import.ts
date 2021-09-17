526ae16f01db959b94e6efb21c4cda453ac1a4c4
Marcelo Kopmann <marcelokopmann@gmail.com>
2021-09-17 17:17:04 -0300

    feat: import timer

--- src/importers/import.ts
+++ src/importers/import.ts
@@ -3,11 +3,14 @@ import ItemsGenerator from "./ItemsGenerator";
 import RecipesGenerator from "./RecipesGenerator";
 import { openFile } from '../utils/files'
 import ZenithParser from "./ZenithParser";
+import { performance } from 'perf_hooks'
 
 const downloadFolder = 'data/raw'
 const generatedFolder = 'data/generated'
 
 async function importData() {
+  const t0 = performance.now();
+
   const lastImportedVersion = openFile(`${downloadFolder}/cdn/version.json`)
   const downloader = new Downloader(downloadFolder)
   const currentLiveVersion = await downloader.getWakfuCdnVersion()
@@ -24,6 +27,10 @@ async function importData() {
   zenithParser.parseAndSaveZenithSublimations()
   const itemsGenerator = new ItemsGenerator(downloadFolder, generatedFolder)
   itemsGenerator.mountItems()
+
+  const t1 = performance.now();
+  const time = ((t1 - t0) / 1000) / 60
+  console.log(`Data importation took ${time.toFixed(2)} minutes.`);
 }
 
 importData()
