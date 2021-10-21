9cf753a623656cf053166d864ff75ebe694b59bf
Marcelo Kopmann <16388408+Markkop@users.noreply.github.com>
2021-10-21 10:14:35 -0300

    feat: shutdown countdown (#49)

--- src/stringsLang.ts
+++ src/stringsLang.ts
@@ -212,11 +212,13 @@ export default {
     fr: 'Monstres trouvés',
     pt: 'Monstros encontrados'
   },
-  donationExtraMessage: {
-    en: '\n\nThis bot might stop running soon, we\'re counting on your donation.\nTo find out more type \`.about\` or click [here](https://www.buymeacoffee.com/markkop)',
-    fr: '\n\nCe bot pourrait bientôt cesser de fonctionner, nous comptons sur votre don.\nPour en savoir plus, tapez \`.about\` ou cliquez [ici](https://www.buymeacoffee.com/markkop)',
-    pt: '\n\nEste bot pode parar de funcionar em breve, contamos com sua doação.\nPara saber mais digite \`.about\` ou clique [aqui](https://www.buymeacoffee.com/markkop)',
-    es: '\n\nEste bot podría dejar de funcionar pronto, contamos con su donación.\nPara obtener más información, escriba \`.about\` o haga clic [aquí](https://www.buymeacoffee.com/markkop)',
+  donationExtraMessage(days: number) {
+      return {
+      en: `\n\nThis bot might stop running in ${days} days, we\'re counting on your donation.\nTo find out more type \`.about\` or click [here](https://www.buymeacoffee.com/markkop)`,
+      fr: `\n\nCe bot pourrait cesser de fonctionner dans ${days} hours, nous comptons sur votre don.\nPour en savoir plus, tapez \`.about\` ou cliquez [ici](https://www.buymeacoffee.com/markkop)`,
+      pt: `\n\nEste bot pode parar de funcionar em ${days} dias, contamos com sua doação.\nPara saber mais digite \`.about\` ou clique [aqui](https://www.buymeacoffee.com/markkop)`,
+      es: `\n\nEste bot podría dejar de funcionar en ${days} días, contamos con su donación.\nPara obtener más información, escriba \`.about\` o haga clic [aquí](https://www.buymeacoffee.com/markkop)`,
+    }
   },
   aboutText: {
     fr: `**Corvo Astral** est un bot Discord qui fournit des informations sur le MMORPG Wakfu.
