202149fdcb0a1ca4a90bbea74af463ce26b833f4
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-02-06 14:59:17 -0300

    refac: about command test

--- tests/about.test.ts
+++ tests/about.test.ts
@@ -1,13 +1,12 @@
-import { Interaction } from 'discord.js'
-import { AboutCommand } from '../src/commands'
+import AboutCommand, { getData } from '../src/commands/About'
 import stringsLang from '../src/stringsLang'
-import { executeCommandAndSpyReply, embedContaining } from './testutils'
+import { executeCommandAndSpyReply, embedContaining, getParsedCommand } from './testutils'
 
 describe('AboutCommand', () => {
   it('replies with about message embed', async () => {
-    const command = {
-      name: 'about'
-    }
+    const commandData = getData('en')
+    const stringCommand = '/about'
+    const command = getParsedCommand(stringCommand, commandData)
     const spy = await executeCommandAndSpyReply(AboutCommand, command)
     expect(spy).toHaveBeenCalledWith(embedContaining({
       color: 0xFFFF00,
